f879b049e15002978698bb62d091a6db8e4af334
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-06-11 18:59:09 +0000

    Initial Empty board test

--- tests/SampleTest.py
+++ tests/SampleTest.py
@@ -7,11 +7,33 @@ from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffRep
 
 
 class Board:
+    def __init__(self):
+        self.board = {}
 
     def print(self, printer):
-        board =(". " * 6 + "\n") * 6
+        board =""
+        for y in range(0,6):
+            for x in range(0,6):
+                if f"{x},{y}" in self.board:
+                    board += "x "
+                else:
+                    board += ". "
+            board += "\n"
+
         printer(board)
 
+    def set_alive(self,x,y):
+        self.board[f"{x},{y}"] = True
+
+    def advance(self):
+        new_board = { (3,1) }
+
+        # see how many live neigbors
+
+        # detirmine next cell
+
+        self.board = new_board
+        pass
 
 
 class RegressionTest(unittest.TestCase):
@@ -21,5 +43,28 @@ class RegressionTest(unittest.TestCase):
         # make a board
         board = Board()
         # approve the board
+        board.print(verify)
+
+    def test_cell_can_be_alive(selfe):
+        # Create board
+        board = Board()
+        board.set_alive(2,1)
+        board.print(verify)
+
+
+    def test_square_advances(selfe):
+        # Create board
+        board = Board()
+        board.set_alive(2,1)
+        board.advance()
+        board.print(verify)
+
+    def test_cell_with_two_neighbours_stays_alive(selfe):
+        # Create board
+        board = Board()
+        board.set_alive(2, 1)
+        board.set_alive(3, 1)
+        board.set_alive(4, 1)
+        board.advance()
         board.print(verify)
 
